9e269620c1e8d0a5cec207141dbedfd3a2d8807b
WangNing-Zhou <1879154660@qq.com>
2023-01-06 07:23:16 +0800

    登录页面搭建

--- sms/src/views/login/index.vue
+++ sms/src/views/login/index.vue
@@ -3,14 +3,14 @@
 
     <el-form :model="form" status-icon :rules="rules" ref="form" label-width="80px" class="login-form">
       <h2 class="login-title">登录</h2>
-      <el-form-item label="账号" prop="uername">
-        <el-input  v-model="form.username" placeholder="请输入账号"></el-input>
+      <el-form-item label="账号" prop="username">
+        <el-input v-model="form.username" placeholder="请输入账号"></el-input>
       </el-form-item>
       <el-form-item label="密码" prop="password">
-        <el-input type="password"  v-model="form.password" placeholder="请输入密码"></el-input>
+        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onsubmit">登录</el-button>
+        <el-button type="primary" @click="onSubmit('form')">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -19,21 +19,55 @@
 <script>
 export default {
   name: "index.vue",
-  data(){
+  data() {
+    let validateUserName = (rule, value, callback) => {
+      value = value.trim() //去除前后空格
+      const length = value && value.length
+      const pwdReg = /^[a-zA-Z0-9_]+$/  //正则
+      if (value === "") {//如果输入的位空的话
+        callback(new Error('请输入账号'))
+      } else if (length < 4 || length > 12) { //账号长度要在4到12位之间
+        callback(new Error('账号长度在4到12位之间'))
+      } else if (!pwdReg.test(value)) {//如果不满足正则的话
+        callback(new Error('账号必须由英文,数字或下划线组成'))
+      } else {//如果上述全部通过
+        callback();
+      }
+    }
     return {
-      form:{
-        username:'',
-        password:''
+      form: {
+        username: '',
+        password: ''
       },
-      reules:{
+      rules: {
+        //为用户名添加规则
+        username: [
+          {
+            required: true,
+            validator: validateUserName,
+            trigger: ['blur', 'change']
+          }
+        ],//required 表示必须输入,validator 验证器 trigger:触发器 值为触发条件
+        //为密码添加规则
+        password: [
+          {
+            required: true,
+            message: '请求输入密码',
+            trigger: ['blur', 'change']
+          }
+        ]
 
       },
     };
 
   },
-  methods:{
-    onsubmit(){
-
+  methods: {
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid)=>{
+        if(valid){
+          console.log(this.form.username,this.form.password)
+        }
+      })
     }
   }
 }
@@ -41,26 +75,25 @@ export default {
 
 <style scoped>
 
-   /*背景的样式*/
-  .login-container{
-    position: absolute;
-    width: 99%;
-    height: 99%;
-    background: url("../../assets/bg.png") no-repeat fixed center;
-    background-size: 100% 100%;
-    overflow: hidden;
-  }
-
-  .login-form{
-    width: 350px;
-    margin: 160px auto;
-    background-color: rgba(255,255,255,0.8) ;
-    padding: 30px;
-    border-radius: 20px;
-  }
-  .login-title{
-    text-align: center;
-  }
-
-
+/*背景的样式*/
+.login-container {
+  position: absolute;
+  width: 99%;
+  height: 99%;
+  background: url("../../assets/bg.png") no-repeat fixed center;
+  background-size: 100% 100%;
+  overflow: hidden;
+}
+/*表单样式*/
+.login-form {
+  width: 350px;
+  margin: 160px auto;
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 30px;
+  border-radius: 20px;
+}
+/*标题样式*/
+.login-title {
+  text-align: center;
+}
 </style>
