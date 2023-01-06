44dba6ef8b1c75eabb07a3187146a4234ba0efeb
WangNing-Zhou <1879154660@qq.com>
2023-01-06 08:05:17 +0800

    使用nodejs编写后端接口

--- ssm-service/routers/index.js
+++ ssm-service/routers/index.js
@@ -12,25 +12,28 @@ const md5 = require('blueimp-md5')
 //     res.send('测试成功')
 // })
 
-router.post('/login',(req,res)=>{
-    const{username,password} = req.body  //结构赋值获取username 与 password
-    UserModel.findOne({username,password:md5(password)})() .then(user=>{ //根据用户名密码查询用户
-        if(user){//登录成功
-            user._doc.role={menus:[]}
-            res.send({status:0,data:user})
-        }else{
-            res.send({status:1,msg:'用户名密码不存在'})
+//post请求, 请求地址 '/login'
+router.post('/login', (req, res) => {
+    const {username, password} = req.body  //结构赋值获取username 与 password
+
+    console.log(req.body);
+    UserModel.findOne({username, password: md5(password)}).then(user => { //根据用户名密码查询用户
+        if (user) {//登录成功
+            user._doc.role = {
+                menus: []
+            }
+            res.send({status: 0, data: user})
+        } else {
+            res.send({status: 1, msg: '用户名密码不存在'})
         }
 
-    }).catch(error=>{
-        console.log('登录异常',error);
-        res.send({status:1,msg:'登录异常'})
+    }).catch(error => {
+        console.log('登录异常', error);
+        res.send({status: 1, msg: '登录异常'})
     })
 
 
 })
-
-
 
 
 module.exports = router
