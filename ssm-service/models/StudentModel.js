b32975eecc96a8cd2c3606200961ad85193f4bc6
WangNing-Zhou <1879154660@qq.com>
2023-01-09 05:46:42 +0800

    学员模板编写

--- ssm-service/models/StudentModel.js
+++ ssm-service/models/StudentModel.js
@@ -4,23 +4,23 @@ const studentSchema = new mongoose.Schema({
 
     name: {type: String, required: true}, //班级名称
     gender: {type: String, required: true}, //授课教师
-    school: String,
-    major: String,
-    grade: String,
-    education: String,
+    school: {type:String},
+    major: {type:String},
+    grade: {type:String},
+    education: {type:String},
     direction: {type: String, required: true},
-    id_number: String,
+    id_number: {type:String},
     phone: {type: String, required: true},
-    parent: String,
-    parent_phone: String, //家长联系电话
-    address: String, //家庭住址
-    qq: String,
+    parent: {type:String},
+    parent_phone: {type:String}, //家长联系电话
+    address:{type:String} , //家庭住址
+    qq: {type:String},
     class: {type: String, required: true},
-    admission_date: String, //入学时间
+    admission_date: {type:String}, //入学时间
     teacher_id: {type: String, required: true},
     manager_id: {type: String, required: true},
-    pictures: Array, //照片列表
-    note: String, //备注
+    pictures:{type:Array}, //照片列表
+    note:{type:String}, //备注
 
 })
 
